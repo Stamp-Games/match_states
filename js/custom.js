d156e7766ef909b791aa69dd6c27706c7bf97f21
Aric Johnson <aric87@comcast.net>
2016-05-12 10:38:22 -0400

    doneski

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,56 @@
 !function(){
+	var states =['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'new_hampshire', 'new_jersey', 'new_mexico', 'new_york', 'north_carolina', 'north_dakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'rhode_island', 'south_carolina', 'south_dakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'west_virginia', 'wisconsin', 'wyoming'];
+
+	var score = 0;
+	var count = 0;
+	function randomNumber() {
+   		return Math.floor(Math.random() * states.length);
+	}
+	
+
+	function switchState(e,ui){
+		$('#correct').show();
+		var currentState = states[randomNumber()]
+		var otherStates =[];
+		var nums = [];
+		while (nums.length < 3){
+			var num = randomNumber();
+			if(nums.indexOf(num) < 0){
+				nums.push(num)
+			}
+		}
+		for(var i in nums){
+			otherStates.push(states[nums[i]])
+		}
+		otherStates.push(currentState);
+		
+		 var scrambledStates = [];
+		 while(otherStates.length){
+		 	scrambledStates.push(otherStates.splice(Math.floor(Math.random() * otherStates.length),1)[0])
+		 }	
+
+		$('#stateImageHolder img').attr('src','/images/'+currentState+'.jpg');
+		$('#stampImageHolder').empty();
+		for(var i in scrambledStates){
+			$('#stampImageHolder').append('<img src=images/'+scrambledStates[i]+'-stamp.jpg class="draggable stamp" >');
+		}
+		$('#stampImageHolder img[src*='+currentState+']').addClass('correct');
+		$('.draggable').draggable({ revert: "invalid" });
+		$( ".droppable" ).droppable( "destroy" );
+		$('.droppable').droppable({
+			drop:switchState,
+			accept:$('img.correct')
+		})
+
+		setTimeout(function(){$('#correct').fadeOut('fast');},1000);
+
+	}
 	$('.droppable').droppable({
-		drop:checkAnswer,
+		drop:switchState,
 		accept:$('img.correct')
 	})
-
-	function checkAnswer(e,ui){
-		console.log(ui)
-	}
-	$('.draggable').draggable({ revert: "invalid" })
+	$('.draggable').draggable({ revert: "invalid" });
+	$('#startOver').click(function(){
+		location.reload();
+	})
 }()
